eda03b4e9865c688811671430f97b004bb069a4d
izzyward02 <iaward@ualr.edu>
2022-01-21 19:33:05 +0000

    UNIT 1

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("Izzy")  # Input statement without a prompt
+print("You Entered: " + firstname)
+
+lastname = input("Enter Your Name:")  # Input statement with a prompt
+print("You Entered: " + lastname)
