77efc8dea903b16c183388e1f4f2d1420c714f07
izzyward02 <iaward@ualr.edu>
2022-02-16 15:05:38 +0000

    UNIT 4 Assignments

--- 04.04 Number of Zeros.py
+++ 04.04 Number of Zeros.py
@@ -1,9 +1,9 @@
 #given 'n' numbers, first num in input is 'n', after that 'n' ints are given
 n = int(input("Enter N: "))
-for i in range(n):
-    num = int(input("Enter a Number: "))
 count = 0
-for num in range(0,0):
+for i in range(n):
+    num = int(input("Enter a Number: "))    #WON'T COUNT THE ZEROS, BUT WILL PROMPT CORRECT NUM OF TIMES
+if num is 0:
     count + 1
 #count num of zeros among given ints and print it (nums equal to zero, not num of zero digits)
 print("Number of Zeros: {}".format(count))
--- 04.05 Number of Factorials.py
+++ 04.05 Number of Factorials.py
@@ -1,4 +1,9 @@
 #given and int 'n', print sum of: 1! + 2! + 3! +...+ N!
 #problem's solution is only one loop (don't use math library)
-#   "Enter N: "
-#   "Sum of Factorials: "
+n = int(input("Enter N: "))
+factSum = 0
+for n in range(1, n + 1):
+    factSum += n*n
+print("Sum of Factorials: {}".format(factSum))
+
+#input n = 4 generates factSum = 30, when it should be 33? ***prob may be on line 6
