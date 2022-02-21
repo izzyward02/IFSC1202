35694ab003e93b17ebd1fa6d419f95718c4de3e7
izzyward02 <iaward@ualr.edu>
2022-02-21 16:04:54 +0000

    UNIT 5 Assignments

--- 05.01 Sum of Sequence.py
+++ 05.01 Sum of Sequence.py
@@ -1,9 +1,8 @@
 #prompt for a sequence of nums, the last num being zero
-num = int(input("Enter a Number (zero to quit): "))
-numSum = 0 
-while num > 0: 
-    numSum += num 
-    int(input("Enter a Number (zero to quit): "))
-else:
-    numSum += 0    
+numSum = 0
+n = 1
+while n > 0:
+    n = int(input("Enter a Number (zero to quit): "))
+    if n > 0:
+        numSum += n
 print(numSum)
--- 05.02 Average of Sequence.py
+++ 05.02 Average of Sequence.py
@@ -1,5 +1,16 @@
 #prompt for a sequence of nums, the last num being zero
-#   "Enter a Number (zero to quit): " <-- prompt x amount of times until user inputs 0
+n = 1.0
+numSum = 0
+avg = 0
+while n > 0:
+    n  = float(input("Enter a Number (zero to quit): "))
+    if n > 0:
+        numSum += n
+    elif numSum != 0: 
+        avg = numSum / n    #error here: float division by zero
+        print("Average: {}".format(avg))
+    else:
+        print("Sequence Length is 0")
 #print the average of the nums
 #   "Average: " <-- should be a float
 #if no data is entered (only a zero was given), generate an error (division by zero)
