ff2506ce6c3b78b5b11a0df4276081431be2eb09
izzyward02 <iaward@ualr.edu>
2022-02-21 15:10:38 +0000

    UNIT 1

--- UNIT 1 Notes/01.00.02 Python Comments.py
+++
@@ -1,9 +0,0 @@
-#This is a comment
-print("Hello, World")
-
-#You can have a comment after a line of code
-print("Hello, World") #This is a comment
-
-#You can "comment out" a line of code to prevent it from executing
-#print("Good Morning")
-print("Hello, World")
--- UNIT 1 Notes/01.00.09 Inputting Data.py
+++
@@ -1,5 +0,0 @@
-firstname = input()  # Input statement without a prompt
-print("You Entered: " + firstname)
-
-lastname = input("Enter Your Name:")  # Input statement with a prompt
-print("You Entered: " + lastname)
