b10559a6c71ebcf70466fe1b85d9a70d3e57923b
izzyward02 <iaward@ualr.edu>
2022-02-21 15:27:42 +0000

    UNIT 5 Assignments

--- 05.01 Sum of Sequence.py
+++ 05.01 Sum of Sequence.py
@@ -1,8 +1,7 @@
 #prompt for a sequence of nums, the last num being zero
 n = 0
-for n in range(n, n + 1):
-    while n > 0:
-        int(input("Enter a Number (zero to quit): "))   
-        n += n
-    else: 
-        print("Sum: {}".format(n))
+while n > 0:
+    int(input("Enter a Number (zero to quit): "))   
+    n += n
+else: 
+    print("Sum: {}".format(n))
