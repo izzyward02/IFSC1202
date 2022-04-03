8b3cedbf5a6af7e01dcba837fd99047e422cef5b
izzyward02 <iaward@ualr.edu>
2022-04-03 03:14:05 +0000

    UNIT 9 Assignments

--- 09.01 Maximum Of A Two Dimensional List.py
+++ 09.01 Maximum Of A Two Dimensional List.py
@@ -1,19 +1,37 @@
 #give two ints for rows & columns (m*n) & subsequent m rows of n elements
 #   input num of rows and columns in list
+user = input("Enter the number of rows and columns: ")
+numList = user.split()
 #   input array
+row = int(numList[0])
+column = numList[1]
 #   print array
+numTable = []
 #   find index pos of max element & print two nums representing index (i*j)
 #       or row num and column num
 #       if multiple elements in diff rows, print one with smaller row num
 #       if multiple elements on same row, print smallest column num
+for i in range(row):
+    row = input(f"Row {i + 1}: Enter {column} values separated by spaces: ")
+    rowNums = row.split()
+    numTable.append(rowNums)
+for i in range(len(numTable)):
+    for j in range(len(numTable[i])):
+        print(numTable[i][j], end = " ")
+    print()
 
-#EXAMPLE OUTPUT:
+maxNum = 0
+c = 1
+r = 1
 
-#Enter the number of rows and columns: 3 4
-#Enter a line of data: 0 3 2 4
-#Enter a line of data: 2 3 5 5 
-#Enter a line of data: 5 1 2 3
-#0 3 2 4 
-#2 3 5 5 
-#5 1 2 3 
-#The maximum value 5 occurred in row 1 column 2
+for i in range(len(numTable)):
+    for j in range(len(numTable[i])):
+        if maxNum < int(numTable[i][j]):
+            maxNum = int(numTable[i][j])
+            maxColumn = c
+            maxRow = r
+        r += 1
+    c += 1
+    r = 1
+
+print("The maximum value {} occurred in row {} column {}".format(maxNum, maxRow, maxColumn))
