08d3fd4bd2269deb1a1038bf41ba534dcfefd184
izzyward02 <iaward@ualr.edu>
2022-04-04 15:09:30 +0000

    UNIT 10 Assignments

---
+++ 10.01 Pets.py
@@ -0,0 +1,16 @@
+#create a class called Pet w/ following attributes:
+#constructor should not have arguments
+#   Name = name of pet
+#   Type = type of pet (Cat, Dog, etc)
+#   Age = age of pet
+#Read 10.01 Pets.txt file
+#   create three pet objects (one per line)
+#   print object attributes for pets using .format() method
+#print Name, Type, Age attributes from each pet object
+
+#EXAMPLE OUTPUT:
+
+#   Name    Type     Age
+#    Fido     Dog       3
+#  Fluffy     Cat       5
+#  Tweety    Bird       4
---
+++ 10.02 Retail Item.py
@@ -0,0 +1,17 @@
+#create class called RetailItem that holds data about item in store
+#class should have following attributes:
+#   Description: description of item (default = "")
+#   UnitsOnHand: num units in inventory (default = 0)
+#   Price: item's retail price (default = 0)
+#create constructor that accepts Description, UnitsOnHand, and Price & sets to attributes in RetailItem class
+#   if one attribute is not present, set attribute to default value
+#create method InventoryValue that returns UnitsOnHand times Price
+#read 10.02 Inventory.txt file & create three objects, one per item
+#display report w/ Description, UnitsOnHand, Price, and InventoryValue
+
+#EXAMPLE OUTPUT:
+
+#Description       Units On Hand               Price     Inventory Value
+#     Jacket                  12                9.95              119.40
+#      Jeans                  40               34.95             1398.00
+#      Shirt                  20               24.95              499.00
