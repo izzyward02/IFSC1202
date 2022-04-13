ad940106105f1d023b4faca55a878259f7b64d19
izzyward02 <iaward@ualr.edu>
2022-04-13 15:49:35 +0000

    UNIT 10 Assignments

--- 10.01 Pets.py
+++ 10.01 Pets.py
@@ -12,25 +12,17 @@ class Pet():
 petTxt = open("10.01 Pets.txt", "r")
 fileRead = petTxt.readline()
 petElement = fileRead.split(",")
-
-while fileRead != "":
-    petElement = fileRead.split(",")
-    fileRead = petTxt.readline()
-petTxt.close()
 #   create three pet objects (one per line)
 furball1= Pet(petElement[0], petElement[1], petElement[2])
-furball2 = Pet(petElement[0][1], petElement[1][1], petElement[2][1])
-furball3 = Pet(petElement[0][2], petElement[1][2], petElement[2][2])
+fileRead = petTxt.readline()
+petElement = fileRead.split(",")
+furball2 = Pet(petElement[0], petElement[1], petElement[2])
+fileRead = petTxt.readline()
+petElement = fileRead.split(",")
+furball3 = Pet(petElement[0], petElement[1], petElement[2])
 #   print object attributes for pets using .format() method
-print("{:<5}{:<5}{:<5}".format("Name", "Type", "Age"))
+print("{:<8}{:<8}{:<8}".format("Name", "Type", "Age"))
 #print Name, Type, Age attributes from each pet object
-print("{:<5}{:<5}{:<5}".format(furball1.Name, furball1.Type, furball1.Age))
-print("{:<5}{:<5}{:<5}".format(furball2.Name, furball2.Type, furball2.Age))
-print("{:<5}{:<5}{:<5}".format(furball3.Name, furball3.Type, furball3.Age))
-
-#EXAMPLE OUTPUT:
-
-#   Name    Type     Age
-#    Fido     Dog       3
-#  Fluffy     Cat       5
-#  Tweety    Bird       4
+print("{:<8}{:<8}{:<8}".format(furball1.Name, furball1.Type, furball1.Age))
+print("{:<8}{:<8}{:<8}".format(furball2.Name, furball2.Type, furball2.Age))
+print("{:<8}{:<8}{:<8}".format(furball3.Name, furball3.Type, furball3.Age))
--- 10.02 Retail Item.py
+++ 10.02 Retail Item.py
@@ -1,13 +1,39 @@
 #create class called RetailItem that holds data about item in store
+class RetailItem():
 #class should have following attributes:
 #   Description: description of item (default = "")
 #   UnitsOnHand: num units in inventory (default = 0)
 #   Price: item's retail price (default = 0)
+    def __init__(self, Description = "", UnitsOnHand = 0, Price = 0):
 #create constructor that accepts Description, UnitsOnHand, and Price & sets to attributes in RetailItem class
 #   if one attribute is not present, set attribute to default value
+        self.Description = Description
+        self.UnitsOnHand = UnitsOnHand
+        self.Price = Price
 #create method InventoryValue that returns UnitsOnHand times Price
+    def InventoryValue(self, UnitsOnHand, Price):
+        InventoryValue = self.UnitsOnHand * self.Price
+        return InventoryValue
 #read 10.02 Inventory.txt file & create three objects, one per item
+fileTxt = open("10.02 Inventory.txt", "r")
+
+fileRead = fileTxt.readline()
+itemElement = fileRead.split(",")
+item1 = RetailItem(itemElement[0], itemElement[1], itemElement[2], InventoryValue)  #CALL LAST PARAMETER SEPARATELY FOR EACH INSTANCE
+
+fileRead = fileTxt.readline()
+itemElement = fileRead.split(",")
+item2 = RetailItem(itemElement[0], itemElement[1], itemElement[2], InventoryValue)
+
+fileRead = fileTxt.readline()
+itemElement = fileRead.split(",")
+item3 = RetailItem(itemElement[0], itemElement[1], itemElement[2], InventoryValue)
 #display report w/ Description, UnitsOnHand, Price, and InventoryValue
+print("{:<8}{:<8}{:<8}{:<8}".format("Description", "Units On Hand", "Price", "Inventory"))
+
+print("{:<8}{:<8}{:<8}{:<8}".format(item1.Description, item1.UnitsOnHand, item1.Price, item1.InventoryValue))
+print("{:<8}{:<8}{:<8}{:<8}".format(item2.Description, item2.UnitsOnHand, item2.Price, item2.InventoryValue))
+print("{:<8}{:<8}{:<8}{:<8}".format(item3.Description, item3.UnitsOnHand, item3.Price, item3.InventoryValue))
 
 #EXAMPLE OUTPUT:
 
