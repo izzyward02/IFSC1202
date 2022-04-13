d279a574e94e0159cf33ed4e181e73f9bf12febd
izzyward02 <iaward@ualr.edu>
2022-04-13 15:17:29 +0000

    UNIT 10 Assignments

--- 10.01 Pets.py
+++ 10.01 Pets.py
@@ -1,34 +1,33 @@
 #create a class called Pet w/ following attributes:
 #constructor should not have arguments
 class Pet():
+    def __init__(self, Name, Type, Age):
 #   Name = name of pet
 #   Type = type of pet (Cat, Dog, etc)
 #   Age = age of pet
-    def __init__(self, name, species, age):
+        self.Name = Name
+        self.Type = Type
+        self.Age = Age
 #Read 10.01 Pets.txt file
-        petTxt = open("10.01 Pets.txt", "r")
-        petRead = petTxt.readline()
-        petList = []
-#   create three pet objects (one per line)
-        self.petName = name
-        self.petType = species
-        self.petAge = age
-#   print object attributes for pets using .format() method
-print("   Name     Type     Age")
-while petRead != "":
-    split = petRead.split(" ")
-    petList.append(split)
-    petRead = petTxt.readline()
+petTxt = open("10.01 Pets.txt", "r")
+fileRead = petTxt.readline()
+petElement = fileRead.split(",")
 
-for a in range(len(petList)):
-    furballName = Pet(petList[a])
-    for b in range(len(petList[a])):
-        furballType = Pet(petList[0][b])
-        for c in range(len(petList[a][b][c])):
-            furballAge = Pet(petList[0][0][c])
-furball = Pet(petList[a][b][c])
+while fileRead != "":
+    petElement = fileRead.split(",")
+    fileRead = petTxt.readline()
+petTxt.close()
+#   create three pet objects (one per line)
+furball1= Pet(petElement[0], petElement[1], petElement[2])
+furball2 = Pet(petElement[0][1], petElement[1][1], petElement[2][1])
+furball3 = Pet(petElement[0][2], petElement[1][2], petElement[2][2])
+#   print object attributes for pets using .format() method
+print("{:<5}{:<5}{:<5}".format("Name", "Type", "Age"))
 #print Name, Type, Age attributes from each pet object
-print("     {}     {}     {}".format(furball.petName, furball.petType, furball.petAge))
+print("{:<5}{:<5}{:<5}".format(furball1.Name, furball1.Type, furball1.Age))
+print("{:<5}{:<5}{:<5}".format(furball2.Name, furball2.Type, furball2.Age))
+print("{:<5}{:<5}{:<5}".format(furball3.Name, furball3.Type, furball3.Age))
+
 #EXAMPLE OUTPUT:
 
 #   Name    Type     Age
