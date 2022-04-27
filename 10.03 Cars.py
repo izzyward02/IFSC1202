c6182c839cf945310ab8f3dc54ab68d23d1090f4
izzyward02 <iaward@ualr.edu>
2022-04-27 21:40:01 +0000

    UNIT 10 Assignments

--- 10.03 Cars.py
+++ 10.03 Cars.py
@@ -3,55 +3,48 @@ class Car():
 #   year
 #   make
 #   speed
-    def __init__(self, Year, Make):
+    def __init__(self, Make, Year, Speed = 0):
 #constructor should accept car's make and year & speed should be set to zero
-        self.Year = Year
         self.Make = Make
-        self.Speed = 0
+        self.Year = Year
+        self.Speed = Speed
 #methods:
-    def Change(Speed, car1):
-        for a in Speed:
-            print(a, end = "\t")
-            if a > 0:
-                car1.Accelerate(a)
-            else:
-                a *= -1
-                car1.Brake(a)
-        return Change
 #   accelerate adds value to car's speed
-    def Accelerate(self, change):
-        self.Speed += change
+    def Accelerate(self):
+        Accelerate = self.Speed + speedData
         return Accelerate
 #   brake subtracts value from car's speed (don't go below zero)
-    def Brake(self, change):
-        if self.Speed < change:
-            change -= self.Speed
-        else:
-            self.Speed -= change
+    def Brake(self):
+        Brake = self.Speed - speedData
         return Brake
 #first record in 10.03 Cars.txt file contains year and make
 fileTxt = open("10.03 Cars.txt", "r")
 carList = []
 carSpeed = []
-
-with open("10.03 Cars.txt", "r") as file:
-    fileRead = fileTxt.readline()
-    carData = fileRead.split("\n")
-    carInfo = carData[0].split(",")
-    for b in carInfo:
-        carList.append(b)
-    yr = carList[0]
-    m = carList[1]
-    for b in range(1, len(carData)):
-        carSpeed.append(carData[b])
+speedChange = 0
 #   instantiate one car object w/ this data
-car1 = Car(yr, m)
-print("Make: {}" + "\n" + "Year: {}".format(car1.Make, car1.Year))
-print("{:>10}{:>10}".format("Change", "Speed"))
+fileRead = fileTxt.readline()
+carData = fileRead.split(", ")
+car1 = Car(carData[0], carData[1])
+print("Make: {}".format(car1.Make))
+print("Year: {}".format(car1.Year))
+
+fileRead = fileTxt.readline()
+print("{:<15}{:<15}".format("Change", "Speed"))
+while fileRead != "":
+    
+    carSpeed.append(fileRead)
 #       if num is greater than zero, call Accelerate
+    if fileRead >= 0:
+        car1.Accelerate()
+        speedChange += fileRead
 #       if num is less than zero, call Brake
+    else:
+        car1.Brake()
+        speedChange -= fileRead
+    fileRead = fileTxt.readline()
 #print change of speed value and current speed
-print("{:>10}{:>10}".format())
+print(carSpeed)
 #EXAMPLE OUTPUT:
 
 #Make: Jeep
