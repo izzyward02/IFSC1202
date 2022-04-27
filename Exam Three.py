f5c795337d32b7d7274118120555fe9b79ee5857
izzyward02 <iaward@ualr.edu>
2022-04-27 15:55:38 +0000

    EXAM THREE

--- Exam Three.py
+++ Exam Three.py
@@ -10,10 +10,10 @@ class House():
 #	methods:
 	def CostPerSqFt(self):
 #		returns cost per SqFt (HINT: Price / SqFt)
-        CostPerSqFt = self.Price / self.SqFt
-        return CostPerSqFt
-
-	def Payment(self, APR, nY):
+		CostPerSqFt = self.Price/self.SqFt
+		CostPerSqFt = round(CostPerSqFt, 2)
+		return CostPerSqFt
+	def Payment(self):
 #		APR = annual percentage rate (HINT: monthly payment, so APR / 12)
 		MPR = APR / 12
 #		nY = num years (HINT: 12 months in year, so numY * 12)
@@ -23,12 +23,8 @@ class House():
 #		NOTE: formula is M = P * (r(1 + r)^n) / ((1 + r)^n - 1)
 #			where M = monthly payment, P = principal (Price of House), r = monthly int rate, n = num payments (# months loan is paid)
 		Payment = P * ((MPR * (1 + MPR) ** nM) / ((1 + MPR) ** nM - 1))
-		return Payment
-#-----------
-#		WORST CASE: return $100 for partial credit (must set up the method)
-#		Payment = 100
-#		return Payment
-#-----------
+		Payment = round(Payment, 2)
+		return Payment				#WONT PRINT PAYMENT BUT VALUE IS CORRECT IN DEBUG
 #open "Exam Three Houses.txt" for reading
 fileTxt = open("Exam Three Houses.txt", "r")
 HouseList = []
@@ -42,16 +38,24 @@ while fileRead != "":
     HouseList.append(houseData)
     fileRead = fileTxt.readline()
 #prompt user for interest rate (NOTE: divide by 100 so x% becomes decimal)
-userIntRate = int(input("Enter Interest Rate: "))
+APR = int(input("Enter Interest Rate: "))
+APR = APR / 100
 #prompt user for num years of mortgage
-userNumYears = int(input("Enter Years for Mortgage: "))
+nY = int(input("Enter Years for Mortgage: "))
+print("\n")
 #loop through HouseList & print Address, SqFt Cost, Price, and Payment for each House obj
 print("{:<20}{:<20}{:<20}{:<20}{:<20}".format("Address", "Sq Ft", "SqFt Cost", "Price", "Payment"))
 
 for a in range(len(HouseList)):
-	 print("{:<20}{:<20}{:<20}{:<20}".format(HouseList[a].Address, HouseList[a].SqFt, HouseList[a].CostPerSqFt, HouseList[a].Price, HouseList[a].Payment()))
+	 print("{:<20}{:<20}{:<20}{:<20}".format(HouseList[a].Address, HouseList[a].SqFt, HouseList[a].CostPerSqFt(), HouseList[a].Price, HouseList[a].Payment))
 print()
 
-#print("{:<20}{:<20}{:<20}{:<20}{:<20}".format(house1.Address, house1.SqFt, house1.CostPerSqFt, house1.Price, house1.Payment))
-#print("{:<20}{:<20}{:<20}{:<20}{:<20}".format(house2.Address, house2.SqFt, house2.CostPerSqFt, house2.Price, house2.Payment))
-#print("{:<20}{:<20}{:<20}{:<20}{:<20}".format(house3.Address, house3.SqFt, house3.CostPerSqFt, house3.Price, house3.Payment))
+#EXPECTED OUTPUT:
+
+#Enter Interest Rate:  6
+#Enter Years for Mortgage: 15
+
+#      Address        Sq Ft      SqFt Cost        Price        Payment
+#    101 Main          1500          66.67       100000         843.86
+#    432 Maple         1750          62.86       110000         928.24
+#    8000 Chenal       3200          87.50       280000        2362.80
