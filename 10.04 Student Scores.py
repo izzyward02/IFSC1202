321a8420ebe56b2ac526d8c555cee1891247bd2d
izzyward02 <iaward@ualr.edu>
2022-04-28 02:38:24 +0000

    UNIT 10 Assignments

--- 10.04 Student Scores.py
+++ 10.04 Student Scores.py
@@ -1,34 +1,62 @@
 #create Python program that calculates running avg, semester avg, and letter grade for students based on variables
-
 #create class Student
+class Student():
 #define initializer, accepting the following class values:
-#   firstName (str)
-#   lastName (str)
-#   tNum (str)
-#   scores (list of string scores) NOTE: this list is variable in length
-#in initializer, create following obj attributes:
-#   FirstName (str)
-#   LastName (str)
-#   TNumber (str)
-#   Grades (list of str grades)
+    def __init__(self, FName, LName, TNum, Scores):
+        self.FName = FName
+        self.LName = LName
+        self.TNum = TNum
+        self.Scores = Scores
 #define methods for obj
 #   RunAvg(self) = calculates running avg (non-blank) of score data
+    def RunAvg(self):
+        Scores = []
+        for grade in self.Scores:
+            if grade != "":
+                Scores.append(int(grade))
+            else:
+                Scores.append(0)
+        RunAvg = sum(Scores) / len(Scores)
+        return RunAvg
 #   TotalAvg(self) = calculates avg of scores, where missing scores are zero
+    def TotalAvg(self):
+        Scores = []
+        for grade in self.Scores:
+            if grade != "":
+                Scores.append(int(grade))
+        TotalAvg = sum(Scores) / len(Scores)
+        return TotalAvg
 #   LetterGrade(self) = returns letter grade based on TotalAvg, where... 
 #       >=90 = A
 #       >=80 and <90 = B
 #       >=70 and <80 = C
 #       >=60 and <70 = D
 #       <60 = F
+    def LetterGrade(self):
+        avg = self.RunAvg()
+        if avg >= 90:
+            return "A"
+        elif avg >= 80:
+            return "B"
+        elif avg >= 70:
+            return "C"
+        elif avg >= 60:
+            return "D"
+        else:
+            return "F"
 #as the program reads each line from StudentScores.txt, create ONE Student obj
 #   reuse obj as the next student data is read
+fileTxt = open("10.04 StudentScores.txt", "r")
+studentList = []
+for a in fileTxt:
+    a = a.strip().split(",")
+    students = Student(a[0], a[1], a[2], a[3:])
+    studentList.append(students)
 #NOTE: Jim E. has 3 scores, second is missing, Joe S. has 4 scores, Jane D. has 2 scores, first is missing
+print("{:>10}{:>10}{:>15}{:>15}{:>15}{:>10}".format("First", "Last", "ID", "Running", "Semester", "Letter"))
+print("{:>10}{:>10}{:>15}{:>15}{:>15}{:>10}".format("Name", "Name", "Number", "Average", "Average", "Grade"))
+print()
+for s in studentList:
+    print("{:>10}{:>10}{:>15}{:>15.2f}{:>15.2f}{:>10}".format(s.FName, s.LName, s.TNum, s.TotalAvg(), s.RunAvg(), s.LetterGrade()))
+print()
 
-#EXAMPLE OUTPUT:
-
-#      First         Last           ID      Running     Semester       Letter
-#        Name         Name       Number      Average      Average        Grade
-#------------ ------------ ------------ ------------ ------------ ------------
-#         Jim        Evans      T123456        83.00        55.33            F 
-#         Joe        Smith      T654321        88.00        88.00            B 
-#        Jane          Doe      T121212        99.50        66.33            D
--- 10.05 Points.py
+++ 10.05 Points.py
@@ -1,6 +1,7 @@
 #use Cartesian coordinate system (x and y axes)
 # each data point has and x- and y-value
 #use the following code to create a datapoint obj... 
+from math import sqrt, pi, atan
 # Step 1 - Define the class object "Point"
 class Point:
 # Step 2 - Define the initializer and any default values
@@ -15,22 +16,48 @@ class Point:
 #create following functions to make calcs about datapoints:
 #   Distance: calcs distance between two points
 #       has parameters Point A and Point B (both are Point objs)
+    def Distance(self, misc):
+        Distance = sqrt((misc.x - self.x) ** 2 + (misc.y - self.y) ** 2)
 #       returns distance as a float 
 #       for points A and B, formula is sqrt((Xb - Xa)^2 + (Yb - Ya)^2)
 #       NOTE: import sqrt function from math library
+        return Distance
 #   MidPoint: calcs midpoint between two datapoints
 #       has parameters Point A and Point B (both are Point objs)
+    def MidPoint(self, misc):
+        MPX = (misc.x + self.x) / 2
+        MPY = (misc.y + self.y) / 2
+        MidPoint = f"({MPX}, {MPY})"
 #       returns midpoint as a Point obj w/ midpoint values
 #       for points A and B, formula is x = (Xb + Xa) / 2 & y = (Yb + Ya) / 2
+        return MidPoint
 #   XAngle: calcs angle of line between two datapoints
 #       has parameters Point A and Point B (both are Point objs)
+    def XAngle(self, misc):
+        slope = (misc.y - self.y) / (misc.x - self.x)
+        XAngle = atan(slope) * 180 / pi
 #       returns angle as a float (formatted in degrees)
 #       for points A and B, formula is slope = (Yb - Ya) / (Xb - Xa) & XAngle = atan(slope) * 180 / pi
 #       NOTE: import atan (arc tangent) function from math library  
+        return XAngle
 #process each line of data in 10.05 Points.txt file
+fileTxt = open("10.05 Points.txt", "r")
+with fileTxt as data:
+    plot = []
+    for a in data:
+        if a != "\n":
+            r = a.split(",")
+            plot.append(r)
+    print(f"{"Point A":^17}{"Point B":^17}{"Distance":^17}{"Midpoint":^17}{"Angle":^17}")
+    print()
 #   as each line reads, create two Point objs & reuse them with next datapoint
 #   first two values are (x,y) for Point A and second two values are (x,y) for Point B
+    for a in range(len(plot)):
+        pA = Point(float(plot[a][0]), float(plot[a][1]))
+        pB = Point(float(plot[a][2]), float(plot[a][3]))
 #read each datapoint pair and display datapoint values, distance, midpoint, and XAngle
+        print(f"{pA.ToString():17}{pB.ToString():17}{pA.Distance(pB):17.7f}{pA.MidPoint(pB):17}{pA.XAngle(pB):<17.7f}")
+print()
 
 #EXPECTED OUTPUT:
 
