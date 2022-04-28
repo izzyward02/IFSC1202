ddf1a9650df3525dbf81abae0f49e5692d70ad6e
izzyward02 <iaward@ualr.edu>
2022-04-28 01:39:59 +0000

    UNIT 10 Assignments

--- 10.03 Cars.py
+++ 10.03 Cars.py
@@ -10,41 +10,45 @@ class Car():
         self.Speed = Speed
 #methods:
 #   accelerate adds value to car's speed
-    def Accelerate(self):
-        Accelerate = self.Speed + speedData
-        return Accelerate
+    def Accelerate(self, fileRead):
+        Accelerate = self.Speed + fileRead
+        return self.Speed
 #   brake subtracts value from car's speed (don't go below zero)
-    def Brake(self):
-        Brake = self.Speed - speedData
-        return Brake
+    def Brake(self, fileRead):
+        Brake = self.Speed + fileRead
+        if self.Speed < 0:
+            self.Speed = 0
+        return self.Speed
 #first record in 10.03 Cars.txt file contains year and make
 fileTxt = open("10.03 Cars.txt", "r")
 carList = []
-carSpeed = []
-speedChange = 0
-#   instantiate one car object w/ this data
 fileRead = fileTxt.readline()
-carData = fileRead.split(", ")
-car1 = Car(carData[0], carData[1])
+while fileRead != "":
+    carData = fileRead.split(", ")
+    carList.append(carData)
+    fileRead = fileTxt.readline()
+#   instantiate one car object w/ this data
+for a in range(1):
+    for b in range(len(carList)):
+        car1 = Car(carData[a][0], carData[a][1])
+        car1.Make = int(carList[a][0])
+        car1.Year = str(carList[a][1])
 print("Make: {}".format(car1.Make))
 print("Year: {}".format(car1.Year))
-
-fileRead = fileTxt.readline()
+#track speed and calculate change of speed
 print("{:<15}{:<15}".format("Change", "Speed"))
-while fileRead != "":
-    
-    carSpeed.append(fileRead)
+for a in range(1, len(carList)):
+    for b in range(1):
+        speedData = float(carList[a][b])
 #       if num is greater than zero, call Accelerate
-    if fileRead >= 0:
-        car1.Accelerate()
-        speedChange += fileRead
+    if speedData > 0:
+        print("{:<20}{:<20}".format(carList[a][b], car1.Accelerate(speedData)))
 #       if num is less than zero, call Brake
-    else:
-        car1.Brake()
-        speedChange -= fileRead
-    fileRead = fileTxt.readline()
+    if speedData < 0:
+        print("{:<20}{:<20}".format(carList[a][b], car1.Brake(speedData)))
 #print change of speed value and current speed
-print(carSpeed)
+print()
+
 #EXAMPLE OUTPUT:
 
 #Make: Jeep
