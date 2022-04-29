79a6dbdc5091ec3c5d9b6f80e0b1a6f1f54a0e14
izzyward02 <iaward@ualr.edu>
2022-04-29 19:31:50 +0000

    UNIT 11 Assignments

--- 11.01 Retail Item.py
+++ 11.01 Retail Item.py
@@ -1,26 +1,52 @@
 #create class RetailItem 
+class RetailItem():
 #   class should have the following attributes:
 #       Description default ("")
 #       UnitsOnHand default (0)
 #       Price default (0)
+    def __init__(self, Description = "", UnitsOnHand = 0, Price = 0):
 #   create constructor of RetailItem that accepts Description, UnitsOnHand, and Price then sets then to attributes in class
 #       if one item is not present, set attribute to default value
+        self.Description = Description
+        self.UnitsOnHand = UnitsOnHand
+        self.Price = Price
 #   create method InventoryValue that returns UnitsOnHand * Price
-#   read 11.01 Inventory.txt & create list of items
-#   print list of items w/ initial inventory value using RetailItem attributes
-#   read 11.01 InventoryUpdate.txt, search for an item to update & update Price
-#   print list w/ updated values
+    def InventoryValue(self):
+        InventoryValue = self.UnitsOnHand * self.Price
+        InventoryValue = round(InventoryValue, 2)
+        return InventoryValue
 #   create print_inventory w/ list as the argument (prints list w/ headings)
+def print_inventory(lists):
+    print("{:>15}{:>15}{:^15}{:>15}".format("Description", "Units On Hand", "Price", "Inventory Value"))
+    for a in range(len(lists)):
+        print("{:>15}{:>15}{:^15}{:>15}".format(lists[a].Description, str(lists[a].UnitsOnHand), str(lists[a].Price), lists[a].InventoryValue()))
+#   print list of items w/ initial inventory value using RetailItem attributes
+    print()
 #   create find_inventory w/ list & name of inventory item as arguments (searches list for a match & returns)
-
-#EXPECTED OUTPUT:
-
-#Description       Units On Hand               Price     Inventory Value
-#     Jacket                  12                9.95              119.40
-#      Jeans                  40               34.95             1398.00
-#      Shirt                  20               24.95              499.00
-
-#Description       Units On Hand               Price     Inventory Value
-#     Jacket                  12               10.95              131.40
-#      Jeans                  40               35.95             1438.00
-#      Shirt                  20               25.95              519.00
+def find_inventory(lists, listItem):
+    for a in range(len(lists)):
+        if lists[a].Description == listItem:
+            return a
+    return -1
+#   read 11.01 Inventory.txt & create list of items
+fileTxtA = open("11.01 Inventory.txt", "r")
+fileRead = fileTxtA.readline()
+itemList = []
+while fileRead != "":
+    itemData = fileRead.split(",")
+    item1 = RetailItem(itemData[0].strip(), float(itemData[1].strip()), float(itemData[2].strip()))
+    itemList.append(item1)
+    fileRead = fileTxtA.readline()
+fileTxtA.close()
+print_inventory(itemList)
+#   read 11.01 InventoryUpdate.txt, search for an item to update & update Price
+fileTxtB = open("11.01 InventoryUpdate.txt", "r")
+fileRead = fileTxtB.readline()
+itemUpdate = []
+while fileRead != "":
+    itemData = fileRead.split(",")
+    itemList[find_inventory(itemList, itemData[0])].Price = float(itemData[2])
+    fileRead = fileTxtB.readline()
+fileTxtB.close()
+print_inventory(itemList)
+print()
--- 11.02 Employee Payroll.py
+++ 11.02 Employee Payroll.py
@@ -1,18 +1,64 @@
 #create class Employee
+class Employee():
 #   create the following attributes:
 #       FirstName
 #       LastName
 #       IDNumber
 #       HoursWorked
 #       Wage
+    def __init__(self, FName, LName, IDNum, HoursWorked, Wage):
 #   create initializer accepting FirstName, LastName, IDNumber, & Wage and assigns them to the attribute
+        self.FName = FName
+        self.LName = LName
+        self.IDNum = IDNum
 #       set HoursWorked to (0)
+        self.HoursWorked = 0
+        self.Wage = Wage
 #   create method WeeklyPay
+    def WeeklyPay(self):
+        hrs = float(self.HoursWorked)
+        wg = float(self.Wage)
 #       1 * Wage for HoursWorked <= 40 & 1.5 * Wage for HoursWorked > 40
+        if hrs > 40:
+            WeeklyPay = (wg * 40) + ((hrs - 40) * (wg * 1.5))
+        else:
+            WeeklyPay = wg * hrs
+        return WeeklyPay
 #   create find_employee w/ employee list and IDNumber as arguments
+def find_employee(listA, IDNum):
+    ind = -1
+    for a in range(len(listA)):
+        if listA[a][0] == IDNum:
+            ind = i
+    return ind
 #   read 11.02 Employees.txt & create a list of objects
+with open("11.02 Employees.txt") as gData:
+    genList = []
+    for g in gData:
+        if g != "\n":
+            g = g.replace(" ", "")
+            g = g.replace("\n", "")
+            data = g.split(",")
+            genList.append(data)
+    print("{:15}{:15}{:15}{:15}{:15}{:15}".format("First Name", "Last Name", "ID Number", "Hours Worked", "Hourly Wage", "Weekly Pay"))
 #   read 11.02 Hours.txt & update hours worked for appropriate employee
+    eList = []
+    for a in range(len(genList)):
+        newE = Employee(genList[a][0], genList[a][1], genList[a][2], genList[a][3])  #MissingArgument but when added IndexOutOfRange
+        eList.append(newE)
+    with open("11.02 Hours.txt") as new:
+        newH = []
+        for n in new:
+            if n != "\n":
+                n = n.replace(",", "")
+                data = n.split(",")
+                newH.append(data)
+        for a in range(len(eList)):
+            ind = find_employee(newH, eList[a].IDNum)
+            if ind != -1:
+                eList[a].HoursWorked = int(newH[ind][1])
 #   print results
+            print("{:15}{:15}{:15}{:15}{:15}{:15}".format(eList[a].FName, eList[a].LName, eList[a].IDNum, eList[a].HoursWorked, eList[a].Wage, eList[a].WeeklyPay()))
 
 #EXPECTED OUTPUT:
 
